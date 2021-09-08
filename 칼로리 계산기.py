6dd156568ac2c5fae7e09889becf969e067adba4
swiss0906 <swiss0906@naver.com>
2021-09-09 05:41:20 +0900

    Create 칼로리 계산기.py

---
+++ 칼로리 계산기.py
@@ -0,0 +1,10 @@
+kitkat = 190
+oreos = 502
+pringles = 292
+twix = 135.9
+cheetos = 485
+
+print(kitkat + oreos * 2)
+print(cheetos * 4)
+print(pringles + oreos + twix)
+print(pringles * 3 + oreos * 2)
