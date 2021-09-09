536102cc8f0f4ee026fd65961c91fbeb7356f87a
swiss0906 <swiss0906@naver.com>
2021-09-09 11:17:08 +0900

    Create 피보나치 수열.py

---
+++ 피보나치 수열.py
@@ -0,0 +1,10 @@
+previous = 0
+current = 1
+i = 1
+
+while i <= 50:
+    print(current)
+    temp = previous  # previous를 임시 보관소 temp에 저장
+    previous = current
+    current = current + temp  # temp에는 기존 previous 값이 저장돼 있음
+    i += 1
